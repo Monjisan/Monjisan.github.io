757524635d6f75d2cd78eb3320590dd4af231ee0
Monjisan <rekkuuzadx@yahoo.co.jp>
2014-08-08 11:43:55 +0900

    add main system

---
+++ index.js
@@ -0,0 +1,21 @@
+﻿$(function(){
+  var tabID = 'tab0';
+
+  function chageTab(){
+    var ID = $(this).attr('id');
+    if(ID!=tabID){
+      $('.tabItem#'+tabID).removeClass('selectTab');
+      $(this).addClass('selectTab');
+      $('.contents#'+tabID+'Contents').css('display','none');
+      $('.contents#'+ID+'Contents').css('display','');
+      tabID = ID;
+    }
+  }
+
+  $('.tabItem').click(chageTab);
+  $('a').each(function(){
+    if($(this).attr('href').indexOf('http')==0){
+      $(this).attr('target', '_blank').append('<span class="comments">[外部リンク]</span>');
+    }
+  });
+});
