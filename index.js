7c14c194ce03a6d892bc0b155b5d0a7b568e5733
Monjisan <rekkuuzadx@yahoo.co.jp>
2014-08-08 23:49:33 +0900

    make twitter page

--- index.js
+++ index.js
@@ -20,7 +20,12 @@
 // init link to out
   $('a').each(function(){
     if($(this).attr('href').indexOf('http')==0){
-      $(this).attr('target', '_blank').append('<span class="comments">[外部リンク]</span>');
+      // open at new tab
+      $(this).attr('target', '_blank');
+      // nc => no comment
+      if(!$(this).hasClass('nc')){
+        $(this).append('<span class="comments">[外部リンク]</span>');
+      }
     }
   });
 });
