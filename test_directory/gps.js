29b639a1746681614a9e29b540e7c410636bcd15
Monjisan <rekkuuzadx@yahoo.co.jp>
2014-11-28 00:34:56 +0900

    accel test

--- test_directory/gps.js
+++ test_directory/gps.js
@@ -7,6 +7,7 @@ var fixed = true;
 var move = new vector();
 var markerMove = false;
 var nearRail;
+var accel = null;
 
 
 // debug variable
@@ -16,7 +17,7 @@ function initialize(){
   //debug function
 exec = function(src){eval(src);};
   var error = function(e){
-    $("#error").html('GPS Error'+(e||""));
+    $("#error").html('Error'+(e||""));
     console.error(e);
   };
 
@@ -25,6 +26,7 @@ exec = function(src){eval(src);};
     navigator.geolocation.getCurrentPosition(cb, error);
   };
   var cb = function(position){
+    // set marker position
     if(markerMove){
       pos = new vector(center.getPosition());
     }else{
@@ -32,13 +34,16 @@ exec = function(src){eval(src);};
       pos = pos.add(move);
     }
 
-    var gl_text = "緯度：" + pos.y + "<br>";
-      gl_text += "経度：" + pos.x + "<br>";
-      gl_text += "高度：" + position.coords.altitude + "<br>";
-      gl_text += "緯度・経度の誤差：" + position.coords.accuracy + "<br>";
-      gl_text += "高度の誤差：" + position.coords.altitudeAccuracy + "<br>";
-      gl_text += "方角：" + position.coords.heading + "<br>";
-      gl_text += "速度：" + position.coords.speed + "<br>";
+    // make text
+    var text = [
+      "緯度：" + pos.y,
+      "経度：" + pos.x,
+      "高度：" + position.coords.altitude,
+      "緯度/経度の誤差：" + position.coords.accuracy,
+      "高度の誤差：" + position.coords.altitudeAccuracy,
+      "方角：" + position.coords.heading,
+      "速度：" + position.coords.speed
+    ];
 
     // update position
     var c = pos.latLng();
@@ -60,11 +65,25 @@ exec = function(src){eval(src);};
     }
     nearRail.setPosition(nearRailPos.latLng());
 
-    $("#disp").html(gl_text);
+    $("#gps").text(text.join("\n"));
 
     // set next action
     setTimeout(call, 1000);
   };
+
+
+  // get acceleration sector
+  var devicemotion = function(e){
+    accel = e.acceleration;
+    var text = [
+      "加速度 X:" + accel.x,
+      "加速度 Y:" + accel.y,
+      "加速度 Z:" + accel.z
+    ];
+    $("#acc").text(text.join("\n"));
+  };
+
+
 
   // create path sector
   var path, d = 0.005, dv = new vector(d,d),
@@ -129,6 +148,9 @@ exec = function(src){eval(src);};
     field.setBounds(new google.maps.LatLngBounds(getpos.sub(dv).latLng(), getpos.add(dv).latLng()));
   };
 
+
+
+
   // set google map
   var mapOptions = {
     center: defPos,
@@ -178,9 +200,15 @@ exec = function(src){eval(src);};
   };
   field = new google.maps.Rectangle(fieldOptions);
 
+
+
+
+
+
   // call main thread
   if (navigator.geolocation) {
     call();
+    window.addEventListener("devicemotion", devicemotion);
     setTimeout(getLine,500);
     $("#fixed").click(function(){
       $(this).text((fixed=!fixed)?"移動開始":"現在地固定");
