8cb0f9015c8401f744d55ba03b42cb902427b966
Monjisan <rekkuuzadx@yahoo.co.jp>
2015-01-14 10:34:44 +0900

    test

--- test_directory/main_new.js
+++ test_directory/main_new.js
@@ -3,6 +3,7 @@ window.addEventListener('load',function(){
   //googlemaps.makeMap($('#map_canvas')[0]);
   gps.on(function(pos, prev){
     console.log("Get Pos", pos);
+    $("#gps").append(pos+'<br>');
     // \•ª‚É—£‚ê‚½‚çXV
     if(pos.dist(prev)>500.0/1000.0){
       openstreetmap.load(pos,function(){
