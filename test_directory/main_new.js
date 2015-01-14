0e2145db6dca7e8cc4070fe84dc5a6203fcbcce2
Monjisan <rekkuuzadx@yahoo.co.jp>
2015-01-14 12:53:29 +0900

    test

--- test_directory/main_new.js
+++ test_directory/main_new.js
@@ -2,6 +2,8 @@ window.addEventListener('load',function(){
   // 描画準備
   var canvas = $('#my_map')[0];
   var ctx = canvas.getContext('2d');
+  var width = canvas.width = canvas.height = 500;
+  var w2 = width/2;
 
   // google maps準備
   googlemaps.makeMap($('#map_canvas')[0]);
@@ -15,8 +17,19 @@ window.addEventListener('load',function(){
     console.log("Get Pos", pos);
     $("#gps").append(pos.lat()+','+pos.lng()+'<br>');
     // 座標に移動
-    googlemaps.center(pos.toGoogle());
+    if(fixed)googlemaps.center(pos.toGoogle());
     center.setPosition(pos.toGoogle());
+    
+    
+    ctx.clearRect(0,0,width,width);
+    openstreetmap.rail.forEach(function(a){
+      ctx.beginPath();
+      a.forEach(function(b){
+        b = pos.toXY(new latLng(b));
+        ctx.lineTo(w2+w2*b.x/d, w2+w2*b.y/d);
+      });
+      ctx.stroke();
+    });
     
     // 十分に離れたら更新
     if(pos.dist(prev)>d){
@@ -32,6 +45,7 @@ window.addEventListener('load',function(){
 
   $("#fixed").click(function(){
     $(this).text((fixed=!fixed)?"移動開始":"現在地固定");
+    if(fixed)googlemaps.center(gps.pos.toGoogle());
   });
   $("#marker_fixed").click(function(){
     $(this).text((markerMove=!markerMove)?"マーカー固定":"マーカー移動開始");
