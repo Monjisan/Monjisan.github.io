143441b8fb4083c5ed392db02169a3bc15aad7ca
Monjisan <rekkuuzadx@yahoo.co.jp>
2015-01-14 11:50:59 +0900

    test

--- test_directory/main_new.js
+++ test_directory/main_new.js
@@ -1,15 +1,21 @@
 window.addEventListener('load',function(){
   
-
+  // 描画準備
   var canvas = $('#my_map')[0];
   var ctx = canvas.getContext('2d');
 
-
-  var d = 500.0/1000.0;
+  // google maps準備
   googlemaps.makeMap($('#map_canvas')[0]);
+  
+  // GPS呼び出し開始
+  var d = 500.0/1000.0;
   gps.on(function(pos, prev){
+    // 座標の表示
     console.log("Get Pos", pos);
     $("#gps").append(pos.lat()+','+pos.lng()+'<br>');
+    // 座標に移動
+    googlemaps.center(pos);
+    
     // 十分に離れたら更新
     if(pos.dist(prev)>d){
       openstreetmap.load(pos, d, function(){
