4d593e7bb34d8ea5f895496c94d915bee634bd14
Monjisan <rekkuuzadx@yahoo.co.jp>
2015-01-16 12:22:25 +0900

    test

--- test_directory/main_new.js
+++ test_directory/main_new.js
@@ -38,7 +38,7 @@ if(!fixed)gps.pos = pos = new latLng(googlemaps.center());
     openstreetmap.rail.forEach(function(way, index){
       var a = way.nodes;
       for(var i=1;i<a.length;++i){
-        var dist = pos.dist(a[i]);//pos.distToLine(a[i],a[i-1]);
+        var dist = 0;//pos.distToLine(a[i],a[i-1]);
         if(dist<nearDist){
           nearDist = dist;
           nearest = index;
